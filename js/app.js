0929177d91a31bcc6f7be12e11c53ca19eafd155
anbav <vibu.anbarasan@gmail.com>
2022-03-02 17:42:51 -0500

    boilerplate

--- js/app.js
+++ js/app.js
@@ -1,6 +1,1 @@
-// console.log('logging...')
-const el = document.createElement('p')
-el.setAttribute('class', 'kolo')
-el.innerHTML = 'Hello Kolo'
-
-document.body.appendChild(el)
+console.log('logging...')
